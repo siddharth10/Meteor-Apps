8592195b01de5f50449183f7eb38be920270dd21
siddharth10 <butusid@gmail.com>
2016-02-23 23:13:53 +0530

    Added files via upload

---
+++ filive-chat.js
@@ -0,0 +1,43 @@
+Messages = new Mongo.Collection('messages');
+
+if (Meteor.isClient) {
+  Template.messages.helpers({
+    messages: function () {
+      return Messages.find({});
+    }
+  });
+  Template.messages.events({
+	  'keypress textarea': function(e, instance){
+		  if(e.keyCode == 13){//enter pressed
+			  e.preventDefault();
+			  var value = instance.find('textarea').value;
+			  instance.find('textarea').value='';
+			  
+			  Messages.insert({
+				  message: value,
+				  timestamp: new Date(),
+				  user: Meteor.userId()
+			  });
+		  }
+	  }
+  });
+  
+  Template.message.helpers({
+	  user: function(){
+		  return Meteor.users.findOne({_id:this.user});
+	  },
+	  time: function(){
+		  return moment(this.timestamp).format('h:mm a');
+	  }
+  });
+  
+  Accounts.ui.config({
+	  passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+  });
+} 
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
